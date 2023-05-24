6f350d0cd630d0ddd717244977506e9d8ba9bc88
maxbarbosa <maxandrade909@gmail.com>
2023-05-23 21:43:50 -0300

    criacao dos formulários do cliente e funcionário

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,12 @@
 from django import forms
-from .models import Veiculo
+from .models import *
 
 class VeiculoForm(forms.ModelForm):
     class Meta:
         model = Veiculo
         fields = ['modelo', 'numportas', 'ano', 'codmarca', 'cor', 'valor']
 
+class FuncionarioForm(forms.ModelForm):
+    class Meta:
+        model = Funcionario
+        fields = ['nome', 'codcargo', 'salario', 'dataadmissao']
